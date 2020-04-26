c6e8c044fb18f1f38ec0439b6314676c5e90e755
yobbad16 <dabboyrey@gmail.com>
2020-04-27 02:33:39 +0800

    Created express server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+// LOAD ENV vars
+dotenv.config({ path: './config/config.env' });
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode  on port ${PORT}!`
+  );
 });
-
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
