750b45f28ff024c614dcb8893fa202b9717d6fb2
yobbad16 <dabboyrey@gmail.com>
2020-04-29 00:21:46 +0800

    Added async handler middleware and add it to bootcamps controller functions

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,94 +1,76 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middlewares/async');
+
 //@desc  GET all bootcamps
 //@route GET /api/v1/bootcamps
 //@access Public
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootCamps = await Bootcamp.find();
-    res
-      .status(200)
-      .json({ success: true, count: bootCamps.length, data: bootCamps });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootCamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ success: true, count: bootCamps.length, data: bootCamps });
+});
 
 //@desc GET single bootcamps
 //@route GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootcamp = async (req, res, next) => {
-  try {
-    const bootCamp = await Bootcamp.findById(req.params.id);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootCamp = await Bootcamp.findById(req.params.id);
 
-    if (!bootCamp) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true, data: bootCamp });
-  } catch (error) {
-    next(error);
+  if (!bootCamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: bootCamp });
+});
 
 //@desc Create new bootcamp
 //@route POST /api/v1/bootcamps/
 //@access Private
-exports.createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      succes: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json({
+    succes: true,
+    data: bootcamp,
+  });
+});
 
 //@desc Update bootcamp
 //@route PUT /api/v1/bootcamps/:id
 //@access Private
-exports.updateBootcamp = async (req, res, next) => {
-  try {
-    const updateBootcamp = await Bootcamp.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    if (!updateBootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-      );
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const updateBootcamp = await Bootcamp.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
     }
+  );
 
-    res.status(200).json({ success: true, data: updateBootcamp });
-  } catch (error) {
-    next(error);
+  if (!updateBootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: updateBootcamp });
+});
 
 //@desc Delete bootcamp
 //@route DELETE /api/v1/bootcamps/:id
 //@access Private
-exports.deleteBootcamp = async (req, res, next) => {
-  try {
-    const deleteBootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const deleteBootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-    if (!deleteBootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: deleteBootcamp });
-  } catch (error) {
-    next(error);
+  if (!deleteBootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: deleteBootcamp });
+});
