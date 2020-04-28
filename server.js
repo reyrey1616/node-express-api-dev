40b0ee229de0a017f78f5f21d88496727002bf38
yobbad16 <dabboyrey@gmail.com>
2020-04-28 16:36:30 +0800

    Created a mongo database and connect the app to database

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middlewares/logger');
+// const logger = require('./middlewares/logger');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
 // LOAD ENV vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
 // INITIALIZED EXPRESS
 const app = express();
+
 // Dev logging middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// app.use(logger);
 
 // MOUNT routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode  on port ${PORT}!`
   );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
