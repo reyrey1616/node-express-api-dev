03f297a9fd950f94dd5c942989b46c4e3e4ed72e
yobbad16 <dabboyrey@gmail.com>
2020-04-30 04:01:35 +0800

    Created delete by id route for courses

--- routes/courses.js
+++ routes/courses.js
@@ -4,10 +4,12 @@ const {
   getCourse,
   addCourse,
   updateCourse,
+  deleteCourse,
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/:id').get(getCourse).put(updateCourse);
 router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+
 module.exports = router;
