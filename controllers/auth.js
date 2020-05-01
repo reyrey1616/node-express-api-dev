c8c4144e03934e07b83a976fb09a9e34e1d6ea88
yobbad16 <dabboyrey@gmail.com>
2020-05-02 02:24:03 +0800

    Created add user to database feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,8 @@ const asyncHandler = require('../middlewares/async');
 //@route GET /api/v1/auth/register
 //@access Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  const user = await User.create({ name, email, password, role });
+
   res.status(200).json({ success: true });
 });
