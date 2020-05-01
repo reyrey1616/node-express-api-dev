b84689ba4508d6c9d47c465ddcf9deb29d297c10
yobbad16 <dabboyrey@gmail.com>
2020-05-02 01:39:30 +0800

    Implement the advanced result middleware to the get bootcamps route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,14 +8,19 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+const advancedResults = require('../middlewares/advancedResults');
 const courseRouter = require('./courses');
+const Bootcamp = require('../models/Bootcamp');
 
 const router = express.Router();
 
 // Re-route into another resource router
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 router.route('/:id/photo').put(bootcampPhotoUpload);
 router
   .route('/:id')
