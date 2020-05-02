87437318846343a87326eb99b8fcaf70ae138f39
yobbad16 <dabboyrey@gmail.com>
2020-05-03 01:38:18 +0800

    Created a relationship between user and courses

--- models/Course.js
+++ models/Course.js
@@ -31,10 +31,16 @@ const CourseSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  //   To create a relation ship in bootcamp. meaning every course must be under the bootcamp
+  //   To create a relationship in bootcamp. meaning every course must be under the bootcamp
   bootcamp: {
     type: mongoose.Schema.ObjectId,
     ref: 'Bootcamp',
+    required: true,
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
     required: true,
   },
 });
