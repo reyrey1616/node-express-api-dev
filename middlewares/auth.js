3ed112bea9eb395ce80a73eff59b2038deb8eba8
yobbad16 <dabboyrey@gmail.com>
2020-05-03 00:38:25 +0800

    Created authorized middleware to grant access to specific role

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -32,3 +32,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     console.error(err);
   }
 });
+
+// Grant Access to specific role
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
