f790348130da7476982fab169e48fd749dbabdc9
yobbad16 <dabboyrey@gmail.com>
2020-05-06 00:42:45 +0800

    Created routes for controllers in users

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const cookieParser = require('cookie-parser');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 // LOAD ENV vars
 dotenv.config({ path: './config/config.env' });
@@ -43,6 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
