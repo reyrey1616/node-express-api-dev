5df1b698b67ba5b6d347856b522aea05e4d67048
yobbad16 <dabboyrey@gmail.com>
2020-05-06 00:27:56 +0800

    Created update user controller for admin

--- controllers/users.js
+++ controllers/users.js
@@ -24,3 +24,14 @@ exports.createUser = asyncHandler(async (req, res, next) => {
   const user = await User.create(req.body);
   res.status(201).json({ success: true, data: user });
 });
+
+//@desc  UPDATE USER
+//@route PUT /api/v1/users/
+//@access Private/Admin
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ success: true, data: user });
+});
