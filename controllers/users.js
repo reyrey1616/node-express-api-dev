5abaa2f6768ec2ad6bc5316d1de358cbb7b321de
yobbad16 <dabboyrey@gmail.com>
2020-05-06 00:26:39 +0800

    Created create user controller for admin

--- controllers/users.js
+++ controllers/users.js
@@ -16,3 +16,11 @@ exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.params.id);
   res.status(200).json({ success: true, data: user });
 });
+
+//@desc  CREATE USER
+//@route POST /api/v1/users/
+//@access Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({ success: true, data: user });
+});
