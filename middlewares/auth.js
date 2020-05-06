db4f6ef94334f9054922b20907fd5e6acd3fbd91
yobbad16 <dabboyrey@gmail.com>
2020-05-06 23:47:00 +0800

    Added the feature that users can access protected routes using JWT stored in cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,15 +7,17 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
+  // If no cookie. the auth middleware will find the token in authorization header
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  // else if(req.cookies.token) {
-  //       token = req.cookies.token;
-  // }
+  // If using cookies. if the token is stored in cookies then will assign it to token and use every request
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   //   Make sure token exists
   if (!token) {
