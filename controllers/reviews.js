f3b4d4c7e522869c415be1d273fe3f643d0e9f6b
yobbad16 <dabboyrey@gmail.com>
2020-05-06 20:09:28 +0800

    Created a controller function for getting single review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -13,9 +13,27 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
     return res.status(200).json({
       success: true,
       count: reviews.length,
-      data: courses,
+      data: reviews,
     });
   } else {
     res.status(200).json(res.advancedResults);
   }
+});
+
+//@desc  GET single review
+//@route GET /api/v1/reviews/:id
+//@access Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No Review found with the id ${re.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({ success: true, data: review });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,12 +10,14 @@ const {
 } = require('../controllers/bootcamps');
 const advancedResults = require('../middlewares/advancedResults');
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 const Bootcamp = require('../models/Bootcamp');
 const { protect, authorize } = require('../middlewares/auth');
 const router = express.Router();
 
 // Re-route into another resource router
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router
   .route('/')
