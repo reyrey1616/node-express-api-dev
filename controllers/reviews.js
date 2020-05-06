47787c748f84eb325768b14d0f2f83623b8bf274
yobbad16 <dabboyrey@gmail.com>
2020-05-06 20:43:07 +0800

    Added route for adding the review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -55,5 +55,5 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
   const review = await Review.create(req.body);
 
-  res.status(200).json({ success: true, data: review });
+  res.status(201).json({ success: true, data: review });
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 const router = express.Router({ mergeParams: true });
 const Review = require('../models/Review');
 const advancedResults = require('../middlewares/advancedResults');
@@ -12,7 +12,8 @@ router
   .get(
     advancedResults(Review, { path: 'bootcamp', select: 'name description' }),
     getReviews
-  );
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 
