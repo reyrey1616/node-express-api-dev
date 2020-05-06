e1c0c031981645e3631f17dbb73d84d52e1251cf
yobbad16 <dabboyrey@gmail.com>
2020-05-06 23:51:33 +0800

    Created route for user logout

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,15 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require('../controllers/auth');
 const { protect } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
+
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.put('/updatepassword', protect, updatePassword);
